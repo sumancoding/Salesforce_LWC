7ecbce48e15518465cb3d6aa615ab64bf2f6788d
Suman Malla <progress.suman@gmail.com>
2022-10-12 09:23:02 +0300

    lwc documentation

--- LWC/force-app/main/default/lwc/helloAgain/helloAgain.js
+++ LWC/force-app/main/default/lwc/helloAgain/helloAgain.js
@@ -1,5 +1,6 @@
 import { LightningElement, wire} from 'lwc';
 import getContactList from '@salesforce/apex/ApexHours.getContactList';
+import getAccountDetails from '@salesforce/apex/ApexHours.getAccountDetails'; //calling imperative method
 
 export default class HelloAgain extends LightningElement {
 greeting="Fuck this shit!!!!";
@@ -7,6 +8,13 @@ Message ="";
 result;
 error;
 
+
+/*
+Get a class
+Create an AuraEnabled Method
+import the method in LWC
+use @wire to call the method // data, error
+*/
 
 @wire(getContactList)
 wiredData({ error, data }) {
@@ -27,7 +35,15 @@ this.Message= e.target.value;
 }
 
 handleSubmit(){
-    alert('Button Working !!')
+    alert('Button Working !!');
+    getAccountDetails()
+      .then(result => {
+        console.log('Result', result);
+      })
+      .catch(error => {
+        console.error('Error:', error);
+    });
+
 }
 
 }
